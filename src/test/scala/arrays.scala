01c5443bc07001616c987032be0f46d9981a2a02
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2017-12-14 17:54:22 +0100

    First test of flatten

--- src/test/scala/arrays.scala
+++ src/test/scala/arrays.scala
@@ -48,3 +48,32 @@ class Tags extends org.scalatest.FunSuite {
     printTag(x)
   }
 }
+
+class Arrays extends org.scalatest.FunSuite {
+
+  /**
+    * Tests whether the array generated with flatten has the correct length,
+    * in the case where the shape of the original array is
+    * ((), (0), (0, 1), ..., (0, 1, ..., n-1))
+    */
+  test("flatten") {
+    val nestedLength: Size     = 1000
+    val numberOfElements: Size = nestedLength * (nestedLength - 1) / 2
+
+    println(numberOfElements)
+
+    val nested = fill(nestedLength)(
+      λ { idx: Index =>
+        fill(idx)(
+          λ { innerIdx: Index =>
+            innerIdx
+          }
+        )
+      }
+    )
+
+    val unnested = WArray.flatten(nested)
+
+    assert(numberOfElements == unnested.length)
+  }
+}
